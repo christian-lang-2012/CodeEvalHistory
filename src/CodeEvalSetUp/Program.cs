c9bccb9ebc915b085027901aa68c91f1134844ff
Christian <clang2012@gmail.com>
2016-06-02 18:24:35 -0500

    - Second Commit fizz buzz

---
+++ src/CodeEvalSetUp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSetUp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var line in File.ReadLines(args[0]))
+            {
+                var numbers = line.Split(' ');
+                var firstNumber = int.Parse(numbers[0]);
+                var secondNumber = int.Parse(numbers[1]);
+                var total = int.Parse(numbers[2]);
+
+                Console.WriteLine(
+                    string.Join(" ",
+                        Enumerable.Range(1, total)
+                            .Select(n => n%(firstNumber*secondNumber) == 0
+                                ? "FB"
+                                : n%firstNumber == 0
+                                    ? "F"
+                                    : n%secondNumber == 0
+                                        ? "B"
+                                        : n.ToString())));
+            }
+        }
+    }
+}
