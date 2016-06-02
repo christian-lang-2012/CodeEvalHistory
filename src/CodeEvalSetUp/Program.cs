b87bfef10e8e882f14bda77a1d3ed0198df6b6ee
Christian <clang2012@gmail.com>
2016-06-02 18:26:34 -0500

    - Print every odd number 1 to 99

--- src/CodeEvalSetUp/Program.cs
+++ src/CodeEvalSetUp/Program.cs
@@ -8,23 +8,10 @@ namespace CodeEvalSetUp
     {
         public static void Main(string[] args)
         {
-            foreach (var line in File.ReadLines(args[0]))
+            var oddNumbers = Enumerable.Range(1, 99).Select(x => x%2 == 1 ? x.ToString() : "");
+            foreach (var oddNumber in oddNumbers)
             {
-                var numbers = line.Split(' ');
-                var firstNumber = int.Parse(numbers[0]);
-                var secondNumber = int.Parse(numbers[1]);
-                var total = int.Parse(numbers[2]);
-
-                Console.WriteLine(
-                    string.Join(" ",
-                        Enumerable.Range(1, total)
-                            .Select(n => n%(firstNumber*secondNumber) == 0
-                                ? "FB"
-                                : n%firstNumber == 0
-                                    ? "F"
-                                    : n%secondNumber == 0
-                                        ? "B"
-                                        : n.ToString())));
+                Console.WriteLine(oddNumber);
             }
         }
     }
