8ea27599c99398a4bb29b13e3dbe5f0f2ca8c6da
Christian <clang2012@gmail.com>
2016-06-02 20:12:24 -0500

    - Sum of nums in file

--- src/CodeEvalSetUp/Program.cs
+++ src/CodeEvalSetUp/Program.cs
@@ -8,27 +8,8 @@ namespace CodeEvalSetUp
     {
         public static void Main(string[] args)
         {
-            foreach (var line in File.ReadLines(args[0]))
-            {
-                if (string.IsNullOrEmpty(line))
-                    continue;
-
-                var nth = int.Parse(line);
-                Console.WriteLine(Fibonacci(nth));
-            }
-        }
-
-        public static int Fibonacci(int n)
-        {
-            var a = 0;
-            var b = 1;
-            for (var i = 0; i < n; i++)
-            {
-                var temp = a;
-                a = b;
-                b = temp + b;
-            }
-            return a;
+            var nums = (from line in File.ReadLines(args[0]) where !string.IsNullOrEmpty(line) select int.Parse(line)).ToList();
+            Console.WriteLine(nums.Sum());
         }
     }
 }
